9cfdec247953d5cf01a5c067df88f00d96fe6fb6
Chema <dev.josemariamm@gmail.com>
2023-08-23 22:52:40 +0200

    Changing current implementation of the url shortening

--- pkg/repository/hash.go
+++ pkg/repository/hash.go
@@ -1,17 +1,22 @@
 package repository
 
 import (
-	"math/rand"
-	"time"
+	"crypto/md5"
+	"io"
 )
 
-var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-
-func generateHash(length int) string {
-	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
-	hash := make([]rune, length)
-	for i := range hash {
-		hash[i] = alphabet[rand.Intn(len(alphabet))]
-	}
-	return string(hash)
+func generateHash(url string) string {
+	h := md5.New()
+	io.WriteString(h, url)
+	return string(h.Sum(nil)) + 
 }
+
+var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+// func generateRandHash(length int) string {
+// 	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+// 	hash := make([]rune, length)
+// 	for i := range hash {
+// 		hash[i] = alphabet[rand.Intn(len(alphabet))]
+// 	}
+// 	return string(hash)
+// }
